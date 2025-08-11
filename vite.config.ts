408e4a3f66b30c0e58525a14a6de34d504ca927d
p0wered <powered.ui@gmail.com>
2025-08-11 17:28:58 +0300

    try fix white screen

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  const isProductionMode = mode === 'production'
+
+  return {
+    plugins: [
+      vue(),
+    ],
+    base: isProductionMode
+        ? '/sites/lk/'
+        : '/',
+    build: {
+      manifest: true,
+      target: [
+        'esnext',
+        'chrome100',
+        'edge100',
+        'firefox100',
+        'safari15'
+      ]
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  }
 })
